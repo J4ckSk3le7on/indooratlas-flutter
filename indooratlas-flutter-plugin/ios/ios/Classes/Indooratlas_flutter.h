e0646ef0270d667c2532f5e52a5fe2ca885eb728
Jorge Greñas <J4ck.DevCode@gmail.com>
2025-08-21 15:52:27 -0500

    Delete iOS

--- indooratlas-flutter-plugin/ios/ios/Classes/Indooratlas_flutter.h
+++
@@ -1,9 +0,0 @@
-#import <Flutter/Flutter.h>
-#import <IndoorAtlas/IALocationManager.h>
-
-@interface IALocationManager ()
-- (void)setObject:(id)obj forKey:(id)key;
-@end
-
-@interface IAFlutterPlugin : NSObject<FlutterPlugin>
-@end
